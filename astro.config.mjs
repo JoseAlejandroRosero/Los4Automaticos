7f136b2e04c7853d7ea8ee6e082ff4bfbc85346f
JSDaleman <jdaleman@unal.edu.co>
2025-06-08 16:26:25 -0500

    Fix: Cambio de error de tipado

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import alpinejs from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apm-2025-1.github.io',
-  base: 'Mep_Mep_Riders_web',
+  base: 'Mep_Mep_Raiders_web',
   vite: {
     plugins: [tailwindcss()],
   },
